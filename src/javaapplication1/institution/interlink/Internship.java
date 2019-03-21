4871e6ceee5ecf617ad130cbbd4ce705b68fcf4a
Andey Yarosh <yarosh.andrey2001@gmail.com>
2019-03-21 18:20:51 +0200

    1. used ArrayList instead of arrays 2. addStudent method with comparing

--- src/javaapplication1/institution/interlink/Internship.java
+++ src/javaapplication1/institution/interlink/Internship.java
@@ -1,31 +1,34 @@
 package institution.interlink;
 
+import java.util.ArrayList;
 import person.Student;
+import institution.University;
 
 public class Internship {
-    
     private String name;
-    private Student[] students = new Student[100];
-    private int countOfStudents;
-    private String allStudents;
+    private ArrayList<Student> students;
     
     // constructor to set up the initial values to an object
     public Internship(String name) {
         this.name = name;
+        students = new ArrayList<Student>();
     }
 
     public void setStudent(Student student) {
-        students[countOfStudents] = student;
-        countOfStudents++;
+        students.add(student);
+    }
+    
+    public void addStudent(Student student, University university) {
+        if(student.getKnowledge().getLevel() > university.getAverage())
+            setStudent(student);
+        else
+            System.out.println(student.getName() + " , sorry, but you can not be added to the intership, please, become more clever and come again ;)\n");
     }
 
-    public String getStudents() {
-        for (int i = 0; i < countOfStudents ; i++) {
-            allStudents += students[i].getName();
-        }
-        return allStudents;
+    public ArrayList<Student> getStudents() {
+        return students;
     }
-
+    
     public String getName() {
         return name;
     }
